56fef3d3432be056c67c8b2e8b762f0a45e31cd6
Ade Chandra Priyatna <ade.chandra.priyatna@gmail.com>
2022-06-22 03:16:32 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace LU_Factorization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_Calc = new System.Windows.Forms.Button();
+            this.btn_Make = new System.Windows.Forms.Button();
+            this.txtBox_Col = new System.Windows.Forms.TextBox();
+            this.txtBox_Row = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtGridView_Ans = new System.Windows.Forms.DataGridView();
+            this.dtGridView_Mtx = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridView_Ans)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridView_Mtx)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dtGridView_Mtx);
+            this.groupBox1.Controls.Add(this.btn_Calc);
+            this.groupBox1.Controls.Add(this.btn_Make);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtBox_Col);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtBox_Row);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dtGridView_Ans);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // btn_Calc
+            // 
+            resources.ApplyResources(this.btn_Calc, "btn_Calc");
+            this.btn_Calc.Name = "btn_Calc";
+            this.btn_Calc.UseVisualStyleBackColor = true;
+            this.btn_Calc.Click += new System.EventHandler(this.btn_Calc_Click);
+            // 
+            // btn_Make
+            // 
+            resources.ApplyResources(this.btn_Make, "btn_Make");
+            this.btn_Make.Name = "btn_Make";
+            this.btn_Make.UseVisualStyleBackColor = true;
+            this.btn_Make.Click += new System.EventHandler(this.btn_Make_Click);
+            // 
+            // txtBox_Col
+            // 
+            resources.ApplyResources(this.txtBox_Col, "txtBox_Col");
+            this.txtBox_Col.Name = "txtBox_Col";
+            // 
+            // txtBox_Row
+            // 
+            resources.ApplyResources(this.txtBox_Row, "txtBox_Row");
+            this.txtBox_Row.Name = "txtBox_Row";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // dtGridView_Ans
+            // 
+            this.dtGridView_Ans.AllowUserToAddRows = false;
+            this.dtGridView_Ans.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dtGridView_Ans, "dtGridView_Ans");
+            this.dtGridView_Ans.Name = "dtGridView_Ans";
+            // 
+            // dtGridView_Mtx
+            // 
+            this.dtGridView_Mtx.AllowUserToAddRows = false;
+            this.dtGridView_Mtx.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dtGridView_Mtx, "dtGridView_Mtx");
+            this.dtGridView_Mtx.Name = "dtGridView_Mtx";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridView_Ans)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridView_Mtx)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dtGridView_Mtx;
+        private System.Windows.Forms.Button btn_Calc;
+        private System.Windows.Forms.Button btn_Make;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBox_Col;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBox_Row;
+        private System.Windows.Forms.DataGridView dtGridView_Ans;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LU_Factorization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Make_Click(object sender, EventArgs e)
+        {
+            int N = int.Parse(txtBox_Row.Text);
+            int M = int.Parse(txtBox_Col.Text);
+
+            dtGridView_Ans.Rows.Clear();
+            dtGridView_Ans.Columns.Clear();
+            dtGridView_Mtx.Rows.Clear();
+            dtGridView_Mtx.Columns.Clear();
+
+            var colA = new DataGridViewColumn();
+            colA.HeaderText = " ";
+            colA.CellTemplate = new DataGridViewTextBoxCell();
+            colA.Width = 40;
+            dtGridView_Ans.Columns.Add(colA);
+            
+            var colA1 = new DataGridViewColumn();
+            colA1.HeaderText = "X Answers";
+            colA1.CellTemplate = new DataGridViewTextBoxCell();
+            colA1.Width = 70;
+            dtGridView_Ans.Columns.Add(colA1);
+
+            for (int i = 0; i < M + 1; i++)
+            {
+                var colMtx1 = new DataGridViewColumn();
+                colMtx1.HeaderText = (i == M ? "B" : "X" + (i + 1).ToString());
+                colMtx1.CellTemplate = new DataGridViewTextBoxCell();
+                colMtx1.Width = 40;
+                dtGridView_Mtx.Columns.Add(colMtx1);
+
+                if (i != M)
+                {
+                    dtGridView_Ans.Rows.Add();
+                }
+               
+            }
+
+            for (int j = 0; j < N; j++)
+            {
+                dtGridView_Mtx.Rows.Add();
+            }
+            
+            for (int k = 0; k < M; k++)
+            {
+                dtGridView_Ans[0, k].Value = "X" + (k + 1).ToString();
+            }
+        }
+
+        private void btn_Calc_Click(object sender, EventArgs e)
+        {
+            double[,] Mtx_A;
+            double[] Mtx_B;
+            double[] Answer;
+            int N = int.Parse(txtBox_Row.Text);
+            int M = int.Parse(txtBox_Col.Text);
+
+            Mtx_A = new double[N, M];
+            Mtx_B = new double[N];
+            Answer = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Mtx_B[i] = Convert.ToDouble(dtGridView_Mtx[N, i].Value);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    Mtx_A[i, j] = Convert.ToDouble(dtGridView_Mtx[j, i].Value);
+                }
+            }
+            //display_Mtx(N, M, Mtx_A, Mtx_B);
+            Answer = LU_Factorization(Mtx_A, Mtx_B, M, N);
+
+            for (int i = 0; i < Answer.Length; i++)
+            {
+                dtGridView_Ans[1, i].Value = Answer[i];
+            }           
+        }
+
+        private double[] LU_Factorization(double[,] A, double[] B, int m, int w)
+        {            
+            int n = A.GetLength(0);
+
+            //Forward Elimination
+            for (int k = 0; k < n - 1; k++)
+            {
+                //Pivotting
+                int p = k;
+                double big = Math.Abs(A[k, k]);
+                for (int ii = k + 1; ii < n; ii++)
+                {
+                    double y = Math.Abs(A[ii, k]);
+                    if (y > big)
+                    {
+                        big = y;
+                        p = ii;
+                    }
+                }
+                if (p != k)
+                {
+                    for (int jj = k; jj < n; jj++)
+                    {
+                        double u = A[p, jj];
+                        A[p, jj] = A[k, jj];
+                        A[k, jj] = u;
+                    }
+                    double o = B[p];
+                    B[p] = B[k];
+                    B[k] = o;
+                }
+
+                for (int i = k + 1; i < n; i++)
+                {
+                    double factor = A[i, k] / A[k, k];
+                    A[i, k] = factor;
+                    for (int j = k + 1; j < n; j++)
+                    {
+                        A[i, j] = A[i, j] - factor * A[k, j];
+                    }
+                    B[i] = B[i] - factor * B[k];
+                }
+            }
+
+            //Forward Substitution
+            double[] x = new double[n];
+            for (int i = 2; i < n; i++)
+            {
+                double sum = B[i];
+                for(int j = 1; j < i-1;i++)
+                {
+                    sum -= A[i, j] * B[i];
+                }
+                B[i] = sum;
+            }
+            //Backward Substitution
+            x[n - 1] = B[n - 1] / A[n - 1, n - 1];
+            for (int i = n - 2; i >= 0; i--)
+            {
+                double sum = 0;
+                for (int j = i + 1; j < n; j++)
+                {
+                    sum += A[i, j] * x[j];
+                }
+                x[i] = (B[i] - sum) / A[i, i];  
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    dtGridView_Mtx[m, i].Value = B[i];
+                    dtGridView_Mtx[j, i].Value = A[i, j];
+                }
+            }
+
+            return x;
+
+        }
+    }
+}
